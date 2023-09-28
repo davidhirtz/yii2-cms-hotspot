5408c0069e9008679f6104c18d91a101f6503266
davidhirtz <david.hirtz@me.com>
2023-09-28 16:10:26 +0200

    Language containers for FR + PT added

--- messages/config.php
+++ messages/config.php
@@ -4,9 +4,9 @@
  * for the Yii framework. It is used by the 'yii message' command.
  */
 return [
-    'sourcePath' => dirname(__DIR__, 2),
+    'sourcePath' => dirname(__DIR__),
     'messagePath' => __DIR__,
-    'languages' => ['de', 'en-US', 'zh-CN', 'zh-TW'],
+    'languages' => ['de', 'en-US', 'zh-CN', 'zh-TW', 'fr', 'pt'],
     'ignoreCategories' => ['anakin', 'cms', 'media', 'newsletter', 'shop', 'yii', 'skeleton'],
     'overwrite' => true,
     'removeUnused' => true,
